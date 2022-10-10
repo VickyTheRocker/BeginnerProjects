733452d7e5fa65863ba4ba4c80a9254141bb036f
Manya Dua <58682587+manyadua16@users.noreply.github.com>
2022-10-10 15:43:43 +0530

    Create classinheader.cpp
    Declaration of classes in header files

---
+++ classinheader.cpp
@@ -0,0 +1,21 @@
+#ifndef DATE_H
+#define DATE_H
+
+class Date
+{
+private:
+    int m_year;
+    int m_month;
+    int m_day;
+
+public:
+    Date(int year, int month, int day);
+
+    void SetDate(int year, int month, int day);
+
+    int getYear() { return m_year; }
+    int getMonth() { return m_month; }
+    int getDay()  { return m_day; }
+};
+
+#endif
